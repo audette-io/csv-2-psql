4ce7ded5d3be04378e03b737f2099bd337fb182c
Dragon Prevost <dragon@audette.io>
2019-02-26 14:37:24 -0800

    implemented argparser in main script

--- csv2psql.py
+++ csv2psql.py
@@ -1,18 +1,27 @@
 from CSV import CSV
 from Database import Database
 
-import sys
+from argparse import ArgumentParser
+
+# Init Argument Parser and Acquire Arguments
+parser = ArgumentParser()
+parser.add_argument('-c', '--copy', dest='copy',const=True, nargs='?', default=False, type=bool, help='Set Copy Flag if PSQL Copy Method Should be Used')
+parser.add_argument('-f', '--file', dest='file', help='Specify File Name of CSV Being Inserted')
+args = parser.parse_args()
 
 def start():
-	
-	csv = CSV()
+	print(args.copy)
+	csv = CSV(args)
 	database = Database(csv.get_schema(), csv.get_table_name())	
 	
 	try:
 		print('Inserting Entries')
-		for entry in csv.get_entries():
-			database.insert(entry)
-		print('\033[92mDone Inserting')
+		if args.copy:
+			database.copy_csv();
+		else:
+			for entry in csv.get_entries():
+				database.insert(entry)
+			print('\033[92mDone Inserting')
 
 	except(Exception) as e:
 		print(e)
